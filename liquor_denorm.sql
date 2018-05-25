494f1da5f054e571e6cf1f2842cfcb403e2ca8cf
Jeff Davis <jeff@highfive.com>
2018-05-25 19:34:54 -0400

    fix liquor_denorm.sql
    restore whole query

--- liquor_denorm.sql
+++ liquor_denorm.sql
@@ -1,14 +1,21 @@
 #standardSQL
 SELECT
-  gstore.county AS county,
-  ROUND(cstore_total/gstore_total * 100,1) AS cstore_percentage
+  county AS county,
+  IF (gstore_total >0,
+    ROUND(cstore_total/gstore_total * 100,1),
+    0) AS cstore_percentage
 FROM (
-  SELECT 
-    county,  
-    sum(total) AS gstore_total 
-  FROM 
+  SELECT
+    county AS county,
+    SUM(IF(cstore IS NULL,
+        total,
+        0)) AS gstore_total,
+    SUM(IF(cstore IS NOT NULL,
+        total,
+        0)) AS cstore_total
+  FROM
     `cpb200_liquor_sales.iowa_sales_denorm`
-  WHERE cstore is null
-  GROUP BY 
+  GROUP BY
     county
-ORDER BY county
+  ORDER BY
+    county)
