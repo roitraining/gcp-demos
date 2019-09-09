4e2321bba56e81c8fef3cc85f14e50d60862ef4b
Jeff Davis <jeff@highfive.com>
2019-09-09 15:18:09 -0700

    quick bq-demo edits
    - re-add line item schema
    - uncomment the bq-load

--- bq-demo/load_data.sh
+++ bq-demo/load_data.sh
@@ -1,9 +1,9 @@
  #!/bin/bash
 
-# bq load --source_format=CSV $DEVSHELL_PROJECT_ID:bq_demo.customer gs://$DEVSHELL_PROJECT_ID-bq-demo/customer* ./customer_schema.json
-# bq load --source_format=CSV $DEVSHELL_PROJECT_ID:bq_demo.order gs://$DEVSHELL_PROJECT_ID-bq-demo/order* ./order_schema.json
-# bq load --source_format=CSV $DEVSHELL_PROJECT_ID:bq_demo.product gs://$DEVSHELL_PROJECT_ID-bq-demo/product* ./product_schema.json
-# bq load --source_format=CSV $DEVSHELL_PROJECT_ID:bq_demo.line_item gs://$DEVSHELL_PROJECT_ID-bq-demo/line_item* ./line_item_schema.json
+bq load --source_format=CSV $DEVSHELL_PROJECT_ID:bq_demo.customer gs://$DEVSHELL_PROJECT_ID-bq-demo/customer* ./customer_schema.json
+bq load --source_format=CSV $DEVSHELL_PROJECT_ID:bq_demo.order gs://$DEVSHELL_PROJECT_ID-bq-demo/order* ./order_schema.json
+bq load --source_format=CSV $DEVSHELL_PROJECT_ID:bq_demo.product gs://$DEVSHELL_PROJECT_ID-bq-demo/product* ./product_schema.json
+bq load --source_format=CSV $DEVSHELL_PROJECT_ID:bq_demo.line_item gs://$DEVSHELL_PROJECT_ID-bq-demo/line_item* ./line_item_schema.json
 
 bq query --use_legacy_sql=false --destination_table=$DEVSHELL_PROJECT_ID:bq_demo.denorm 'SELECT
   c.*,
