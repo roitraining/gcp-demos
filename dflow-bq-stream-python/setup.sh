6db5c76ff181d4b6dbbfaa051f90eb88495de43f
Jeff Davis <jwdavis@users.noreply.github.com>
2021-01-05 20:19:22 -0800

    readme and setup

--- dflow-bq-stream-python/setup.sh
+++ dflow-bq-stream-python/setup.sh
@@ -2,6 +2,8 @@
 
 export PROJECT_ID=$(gcloud config get-value project)
 
+sudo apt-get install python3-venv -q -y
+pip install wheel
 python3 -m venv env
 source env/bin/activate; 
 pip install -r req1.txt
@@ -13,9 +15,9 @@ export sa_email=$(gcloud iam service-accounts list --filter="displayName:$1" --f
 gcloud projects add-iam-policy-binding $PROJECT_ID\
     --member="serviceAccount:$sa_email" \
     --role="roles/editor"
-gcloud iam service-accounts keys create demo_sa.json --iam-account=$sa_email
-export GOOGLE_APPLICATION_CREDENTIALS="${1-demo_.json}"
+gcloud iam service-accounts keys create $1.json --iam-account=$sa_email
+export GOOGLE_APPLICATION_CREDENTIALS=$1.json
 
 gsutil mb -l us-central1 "gs://$PROJECT_ID-dflow-demo"
 
-gcloud services enable pubsub, bigquery
+gcloud services enable pubsub bigquery dataflow
