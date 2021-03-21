6834fe68b029a3e6a0d1c212e0af3ff78335edba
Jeff Davis <jeff@highfive.com>
2021-03-20 19:11:53 -0700

    changed project 2

--- bq-demo/load_data.sql
+++ bq-demo/load_data.sql
@@ -11,17 +11,17 @@ SELECT
   prod_desc,
   prod_price
 FROM
-  `roi-bq-demo.bq_demo.customer` c
+  `roi-bq-demos.bq_demo.customer` c
 LEFT JOIN
-  `roi-bq-demo.bq_demo.order` o
+  `roi-bq-demos.bq_demo.order` o
 ON
   c.cust_id = o.cust_id
 LEFT JOIN
-  `roi-bq-demo.bq_demo.line_item` AS li
+  `roi-bq-demos.bq_demo.line_item` AS li
 ON
   o.order_num = li.order_num
 LEFT JOIN
-  `roi-bq-demo.bq_demo.product` AS p
+  `roi-bq-demos.bq_demo.product` AS p
 ON
   li.prod_code = p.prod_code);
 
