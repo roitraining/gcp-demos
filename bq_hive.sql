a7b17abf994865b24380c48c8ceb5be8baee449c
Jeff Davis <jwdavis@users.noreply.github.com>
2022-05-23 11:59:16 -0700

    norm codelab

--- bq_hive.sql
+++ bq_hive.sql
@@ -1,16 +1,3 @@
--- CREATE A PERMANENT EXTERNAL TABLE (console)
--- 1. select your dataset 
--- 2. from three-dot menu, select create table
--- 3. in Create table from, select Google Cloud Storage
--- 4. in Select file... enter jwd-gcp-demos/orders_partitioned/*
--- 5. in File format select parquet
--- 6. select Source data partitioning
--- 7. in Select Source... enter gs://jwd-gcp-demos/orders_partitioned/
--- 8. in Table, enter ext_part 
--- 9. in Table type select External table
--- 10. in Schema, select Auto detect
--- 11. Click CREATE TABLE
-
 -- query the external table
 SELECT
   *
