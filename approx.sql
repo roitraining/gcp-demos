d30ef576a3954d3f9664cd5fb69a9747ffceffbc
Jeff Davis <jwdavis@users.noreply.github.com>
2022-06-08 13:01:55 -0700

    adding pictures to the dataflow DIN

--- approx.sql
+++ approx.sql
@@ -3,21 +3,21 @@
 -- When it's done, run the exact
 -- Compare times and % difference
 
--- First query - approx
--- StandardSQL
--- wiki 1M
-SELECT
-  approx_count_distinct(title) AS articles
-FROM
-  `bigquery-samples.wikipedia_benchmark.Wiki100B`
-ORDER BY
-  articles DESC
-
 -- First query - exact
 -- StandardSQL
 -- wiki 1M
 SELECT
   COUNT(distinct title) AS articles
+FROM
+  `bigquery-samples.wikipedia_benchmark.Wiki100B`
+ORDER BY
+  articles DESC
+
+-- Second query - approx
+-- StandardSQL
+-- wiki 1M
+SELECT
+  approx_count_distinct(title) AS articles
 FROM
   `bigquery-samples.wikipedia_benchmark.Wiki100B`
 ORDER BY
